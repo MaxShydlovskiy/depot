4fcd0206427ffa27ac348c16eb8f110b2eeb58d3
Max Shydlovskiy <shidlymax@gmail.com>
2023-01-09 13:37:56 +0200

    Connecting to a Slow Payment  with Active Job

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -32,7 +32,8 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-        OrderMailer.received(@order).deliver_later
+        ChargeOrderJob.perform_later(@order,pay_type_params.to_h)
+        # @order.charge!(pay_type_params) # do not do this
         format.html { redirect_to store_index_url, notice:
           'Thank you for your order.' }
         format.json { render :show, status: :created, location: @order }
