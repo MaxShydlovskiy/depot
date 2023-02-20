d53b7894dc710338ee705416934d9263a1d16da0
Max Shydlovskiy <shidlymax@gmail.com>
2023-02-20 13:39:06 +0200

    - форма конфірму

--- app/controllers/payment_intents_controller.rb
+++ app/controllers/payment_intents_controller.rb
@@ -1,7 +1,7 @@
 require 'stripe'
 
 class PaymentIntentsController < ApplicationController
-  skip_before_action :authorize, only: [:confirm, :create]
+  skip_before_action :authorize, only: [:create]
   def create
     Stripe.api_key = Rails.application.credentials.stripe_secret_key
 
