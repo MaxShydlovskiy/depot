09f90f54592a8c08d2c88b5ea70f1c87a28b5207
Max Shydlovskiy <shidlymax@gmail.com>
2023-02-20 20:09:45 +0200

    1

--- app/controllers/payment_intents_controller.rb
+++ app/controllers/payment_intents_controller.rb
@@ -15,7 +15,12 @@ class PaymentIntentsController < ApplicationController
 
   def confirm
     Stripe.api_key = Rails.application.credentials.stripe_secret_key
-    payment_confirm = Stripe::PaymentIntent.confirm
+    payment_confirm = Stripe::PaymentIntent.confirm(
+      params[:payment_intent_id],
+      {
+        payment_method: params[:card]
+      }
+    )
 
     render json: payment_confirm
   end
