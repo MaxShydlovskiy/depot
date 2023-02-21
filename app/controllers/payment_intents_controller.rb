8f0046c144637d7e7f7f132d0ce3f97888feb87e
Max Shydlovskiy <shidlymax@gmail.com>
2023-02-21 12:34:36 +0200

    Last commit

--- app/controllers/payment_intents_controller.rb
+++ app/controllers/payment_intents_controller.rb
@@ -2,13 +2,14 @@ require 'stripe'
 
 class PaymentIntentsController < ApplicationController
   skip_before_action :authorize, only: [:create, :confirm]
+  skip_before_action :verify_authenticity_token, only: [ :confirm ]
   def create
     Stripe.api_key = Rails.application.credentials.stripe_secret_key
 
     payment_intent = Stripe::PaymentIntent.create({
       amount: params[:amount],
       currency: 'usd',
-      payment_method: params[:payment_method_id],
+      payment_method: params[:payment_method_id]
     })
     render json: payment_intent
   end
